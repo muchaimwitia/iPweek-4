67035362921ead4029b023126ca40391c6a3181d
muchai mwitia <muchairiungu@gmail.com>
2020-03-02 13:13:39 +0300

    Correction of js

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,20 @@
-function getValue() {
-  var selectedValue = document.getElementById("Size").value;
-  return parseInt(selectedValue);
+function getSize() {
+  var selectedSize = document.getElementById("Size").value;
+  return parseInt(selectedSize);
 }
 function getToppingsDensity() {
-  var selectedCrust = document.getElementById("Toppings density").value;
+  var selectedToppingsDensity = document.getElementById("ToppingsDensity").value;
   return parseInt(selectedToppingsDensity);
 }
 function getCrustThickness() {
-  var selectedToppings = document.getElementById("Crust thickness").value;
+  var selectedCrustThickness = document.getElementById("CrustThickness").value;
   return parseInt(selectedCrustThickness);
 }
 function getNumber() {
   var selectedNumber = document.getElementById("Number").value;
   return parseInt(selectedNumber);
 }
-function getTotal() {
-  var totalAmount = (getValue() + getCrustThickness() + getToppingsThickness()) * getTotal();
-  alert("You have successfully orderd" + getQuantity() + " pizza(s) from Pizzeria Laventino." + "" + " The Total Amount is " + (totalAmount) + "" + " Thank you and Come again!");
+function getValue() {
+  var totalAmount = (getSize() + getCrustThickness() + getToppingsDensity()) * getNumber();
+  alert("You have successfully orderd" + getNumber() + " pizza(s) from Pizzeria Laventino." + "" + " The Total Amount is " + (totalAmount) + "" + " Thank you and Come again!");
 }
